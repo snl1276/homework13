2680de7f046a3e2fedbd5f71d5ca591ca17720b7
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-04-03 20:31:57 +0300

    changed main2.py

--- main2.py
+++ main2.py
@@ -1,6 +1,5 @@
 from sqlalchemy import insert, select
 from db import Base, engine, execute
-from models import User, Address
+from models import User, Address, Gender_age
 
 Base.metadata.create_all(engine)
-
